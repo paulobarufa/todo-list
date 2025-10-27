efabca267acefa27b8eb6e41e78f65e01238e233
bigbarufa <paulo.matos16@gmail.com>
2025-10-27 12:42:01 +0000

    Added JS to HTMLwriter
    Added functionality to HTMLwriter class to create left panel HTML dynamically from project list

--- src/HTMLwriter.js
+++ src/HTMLwriter.js
@@ -1,3 +1,43 @@
+import projectIcon from "./images/project.svg"
+import taskIcon from "./images/task.svg"
+import checkIcon from "./images/check.svg"
+
 export class HTMLwriter {
-    
+
+    static generateCol(projArray) {
+
+        let newWrapper = document.createElement("div");
+
+        for (const project of projArray) {
+            let projDiv = document.createElement("div");
+            projDiv.classList.add("col-item", "proj-col-title");
+
+            const projTitle = document.createTextNode(project.name);
+
+            const projImage = document.createElement("img");
+            projImage.src = projectIcon;
+            projImage.classList.add("col-icon");
+
+            projDiv.append(projImage, projTitle);
+
+            newWrapper.appendChild(projDiv);
+
+            for (const task of project.tasks) {
+                let taskDiv = document.createElement("div");
+                taskDiv.classList.add("col-item", "task-col-title");
+
+                const taskTitle = document.createTextNode(task.name);
+                
+                const taskImg = document.createElement("img");
+                taskImg.src = taskIcon;
+                taskImg.classList.add("col-icon", "task-icon");
+                
+                taskDiv.append(taskImg, taskTitle);
+
+                newWrapper.appendChild(taskDiv);
+            }
+        }
+
+        return newWrapper;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -5,3 +5,29 @@ import { Task } from "./task";
 import { StorageWriter } from "./storage";
 import { HTMLwriter } from "./HTMLwriter";
 
+const dynamicWrapper = document.querySelector(".dynamic-wrapper");
+
+let proj1 = new Project("First Project", "This is the first project.");
+
+let task1 = new Task("Task 1", "Task task task", "tmrw", proj1, 1);
+let task2 = new Task("Task 2", "Task task task", "tmrw", proj1, 1);
+let task3 = new Task("Task 3", "Task task task", "tmrw", proj1, 1);
+
+proj1.addTask(task1)
+proj1.addTask(task2)
+proj1.addTask(task3)
+
+let proj2 = new Project("Second Project", "This is the first project.");
+
+let task4 = new Task("Task 1", "Task task task", "tmrw", proj2, 1);
+let task5 = new Task("Task 2", "Task task task", "tmrw", proj2, 1);
+
+proj2.addTask(task4)
+proj2.addTask(task5)
+
+const projArray = [proj1, proj2];
+
+const newWrapper = HTMLwriter.generateCol(projArray);
+
+dynamicWrapper.appendChild(newWrapper);
+
